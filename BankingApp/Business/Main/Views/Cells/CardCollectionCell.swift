935b71da537e44d1dfedf281c9a0570b2a92d72e
narmin-b <bgvnrm@icloud.com>
2024-11-14 23:28:25 +0400

    card view added

--- BankingApp/Business/Main/Views/Cells/CardCollectionCell.swift
+++ BankingApp/Business/Main/Views/Cells/CardCollectionCell.swift
@@ -8,13 +8,56 @@
 import UIKit
 
 final class CardCollectionCell: UICollectionViewCell {
-
+    private lazy var cardImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+//        imageView.backgroundColor = .red
+        imageView.layer.cornerRadius = 12
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var cardTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16, weight: .semibold)
+        label.textColor = .label
+        label.text = "Card"
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configureView()
     }
         
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-  
+    
+    func configureCell(color: UIColor) {
+        cardImageView.backgroundColor = color
+    }
+    
+    fileprivate func configureView() {
+        cardImageView.addSubview(cardTitleLabel)
+        addSubview(cardImageView)
+        
+        configureConstraints()
+    }
+    
+    fileprivate func configureConstraints() {
+        cardImageView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        cardImageView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        cardImageView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        cardImageView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        cardImageView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        cardImageView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        cardImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        cardImageView.widthAnchor.constraint(equalToConstant: 340).isActive = true
+        
+        cardTitleLabel.centerXAnchor.constraint(equalTo: cardImageView.centerXAnchor).isActive = true
+        cardTitleLabel.centerYAnchor.constraint(equalTo: cardImageView.centerYAnchor).isActive = true
+    }
 }
--- BankingApp/Business/Main/Views/Controllers/MainViewController.swift
+++ BankingApp/Business/Main/Views/Controllers/MainViewController.swift
@@ -8,11 +8,13 @@
 import UIKit
 
 class MainViewController: BaseViewController {
+    let color = [UIColor.red, UIColor.blue]
+    
     private lazy var profileIcon: UIImageView = {
         let imageView = UIImageView()
         imageView.image = UIImage(systemName: "person.crop.circle")
         imageView.tintColor = .black
-        imageView.contentMode = .scaleAspectFill
+        imageView.contentMode = .scaleToFill
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
@@ -30,10 +32,36 @@ class MainViewController: BaseViewController {
         stack.axis = .horizontal
         stack.alignment = .leading
         stack.distribution = .fillProportionally
-        stack.spacing = 16
-        stack.backgroundColor = .blue
+        stack.spacing = 4
         stack.translatesAutoresizingMaskIntoConstraints = false
         return stack
+    }()
+    
+    private lazy var cardView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var cardCollection: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.estimatedItemSize = CGSize(width: 64, height: 24)
+        layout.itemSize = CGSize(width: 64, height: 24)
+        layout.scrollDirection = .horizontal
+        layout.minimumInteritemSpacing = 16
+        layout.minimumLineSpacing = 20
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.backgroundColor = .white
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.isPagingEnabled = true
+        collectionView.contentInset = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.register(CardCollectionCell.self, forCellWithReuseIdentifier: "CardCollectionCell")
+
+        return collectionView
     }()
 
     override func viewDidLoad() {
@@ -41,25 +69,47 @@ class MainViewController: BaseViewController {
         isLoggedIn()
         
         configureView()
-        
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+
+        self.navigationController?.navigationBar.isHidden = true
     }
     
     override func configureView() {
         view.addSubview(profileStack)
+        cardView.addSubview(cardCollection)
+        view.addSubview(cardView)
         
         configureConstraint()
     }
     
     override func configureConstraint() {
         NSLayoutConstraint.activate([
-            profileStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24),
+            profileStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 4),
             profileStack.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
-            profileStack.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20),
+//            profileStack.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20),
+            profileStack.heightAnchor.constraint(equalToConstant: 36),
+            profileIcon.widthAnchor.constraint(equalToConstant: 36),
             profileIcon.leftAnchor.constraint(equalTo: profileStack.leftAnchor, constant: 0),
             profileIcon.centerYAnchor.constraint(equalTo: profileStack.centerYAnchor),
             profileInfo.centerYAnchor.constraint(equalTo: profileStack.centerYAnchor),
 
         ])
+        
+        NSLayoutConstraint.activate([
+            cardView.topAnchor.constraint(equalTo: profileStack.bottomAnchor, constant: 20),
+            cardView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 0),
+            cardView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: 0),
+            cardView.heightAnchor.constraint(equalToConstant: 280),
+            
+            cardCollection.topAnchor.constraint(equalTo: cardView.topAnchor, constant: 20),
+            cardCollection.leftAnchor.constraint(equalTo: cardView.leftAnchor, constant: 12),
+            cardCollection.rightAnchor.constraint(equalTo: cardView.rightAnchor, constant: -12),
+            cardCollection.bottomAnchor.constraint(equalTo: cardView.bottomAnchor, constant: -20)
+        ])
+        
     }
     
     fileprivate func isLoggedIn() {
@@ -69,3 +119,15 @@ class MainViewController: BaseViewController {
     }
 }
 
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCollectionCell", for: indexPath) as! CardCollectionCell
+        cell.configureCell(color: color[indexPath.row])
+        return cell
+    }
+    
+}
